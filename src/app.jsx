753877053932bffb2588e188b58f652db7c826fd
tantaka <tomokazu.tantaka@glpgs.com>
2023-03-03 22:08:07 +0900

    aa

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,67 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
-import './app.css'
+// import './app.css'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" class="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://preactjs.com" target="_blank">
+    //       <img src={preactLogo} class="logo preact" alt="Preact logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + Preact</h1>
+    //   <div class="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/app.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p class="read-the-docs">
+    //     Click on the Vite and Preact logos to learn more
+    //   </p>
+    // </>
+
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+      <div class="card" style="width: 18rem;">
+        <img src={preactLogo} class="card-img-top" alt="Preact logo" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
     </>
+
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser',
+      },
+      {
+        find: 'src',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ],
+  },
 })
